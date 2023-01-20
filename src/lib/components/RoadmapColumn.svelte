bfdae0c4f0945afab2ce80c5486b51c1548a6273
Glen Ottley <glenottley@gmail.com>
2023-01-20 14:54:34 -0500

    roadmap complete

---
+++ src/lib/components/RoadmapColumn.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ProductRequestRoadmap from '$components/ProductRequestRoadmap.svelte'
+	import { fly } from 'svelte/transition'
+
+	export let status
+</script>
+
+<div in:fly={{ x: 200, duration: 400 }}>
+	<h2 class="mb-1 text-lg font-bold text-gray-500 md:text-[14px]">
+		{status.title}
+		({status.productRequests.length})
+	</h2>
+	<h3 class="text-xs text-gray-400 mb-6 md:text-[14px]">{status.description}</h3>
+	<div class="flex flex-col gap-4">
+		{#each status.productRequests as productRequest (productRequest.id)}
+			<ProductRequestRoadmap {productRequest} color={status.color} />
+		{/each}
+	</div>
+</div>
---
+++ src/lib/components/RoadmapNav.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let statusObj
+	export let pageStatus
+</script>
+
+<nav class="relative border-b border-gray-300 ">
+	<ul class="container flex relative justify-between items-center">
+		{#each Object.values(statusObj) as status}
+			<li class="flex flex-col w-full">
+				<a
+					href="/roadmap?status={status.slug}"
+					class="text-xs font-bold text-center py-4 w-full cursor-pointer 
+        {status.slug === pageStatus ? 'text-gray-500' : 'text-gray-300'}"
+					>{status.title} ({status.productRequests.length})</a
+				>
+				{#if status.slug === pageStatus}
+					<span class="h-1 w-full bg-{status.color}" />
+				{:else}
+					<span class="h-1 w-full bg-transparent" />
+				{/if}
+			</li>
+		{/each}
+	</ul>
+</nav>
